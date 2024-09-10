bedecca73ecb3ae77f88fa9e59d55dab0987ea3b
stayFoolish <nemanjakaraklajic90@icloud.com>
2024-09-10 19:37:50 +0200

    Chapter 02

---
+++ chapter-02/src/components/VerySlowComponent.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { wait } from "../utils/wait.ts";
+
+const VerySlowComponent: React.FC = () => {
+  console.log("VerySlowComponent render!");
+
+  wait(400);
+  return null;
+};
+
+export default VerySlowComponent;
